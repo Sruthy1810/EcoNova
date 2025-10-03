649352e66e60554c34e6356cf6cf15b961cef17c
SRUTHY R <117913189+Sruthy1810@users.noreply.github.com>
2025-10-03 18:04:56 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import RepairAdvisor from "./pages/RepairAdvisor";
+import GreenShopping from "./pages/GreenShopping";
+import LifecycleTracker from "./pages/LifecycleTracker";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/repair" element={<RepairAdvisor />} />
+          <Route path="/shop" element={<GreenShopping />} />
+          <Route path="/tracker" element={<LifecycleTracker />} />
+          <Route path="/about" element={<About />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
